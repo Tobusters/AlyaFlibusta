508ace15a75516dd1c1b5217cf4980e89850f217
sm0let <motivan81@gmail.com>
2025-02-25 12:05:33 -0800

    #

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,8 +33,8 @@ namespace WpfApp1
 		USERS users = USERS.getInstance();
 		List<string> SelectedGenreId = new List<string>();
 		//SQLConnection conn = SQLConnection.getInstance(@"Server=DESKTOP-UNTJG88\SQLEXPRESS;database=AlyaFlibusta;Integrated Security=true;Trusted_Connection=true;TrustServerCertificate=true");//под ето отдельный поток нужно кидать
-		//SQLConnection conn = SQLConnection.getInstance();//под ето отдельный поток нужно кидать
-		SQLConnection conn = SQLConnection.getInstance(@"Server=DESKTOP-CVTHJDK\SQLEXPRESS;database=AlyaFlibusta2;Integrated Security=true;Trusted_Connection=true;TrustServerCertificate=true");//под ето отдельный поток нужно кидать
+		SQLConnection conn = SQLConnection.getInstance();//под ето отдельный поток нужно кидать
+														 //SQLConnection conn = SQLConnection.getInstance(@"Server=DESKTOP-CVTHJDK\SQLEXPRESS;database=AlyaFlibusta2;Integrated Security=true;Trusted_Connection=true;TrustServerCertificate=true");//под ето отдельный поток нужно кидать
 		RegLog reglog = new RegLog(); 
 		#endregion
 
